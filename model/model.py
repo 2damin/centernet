4f2ec54532c387b99076bcc1b86af8270174a9c8
2damin <leesak2003@naver.com>
2021-06-08 12:49:51 +0000

    WIP : make TransposedConv2d

--- model/model.py
+++ model/model.py
@@ -42,6 +42,63 @@ class ResNet(nn.Module):
         x = nn.Softmax(x)
         return x
 
+class CenterNet_ResNet(nn.Module):
+    def __init__(self, n_channels, n_classes ):
+        super(ResNet, self).__init__()
+        self.n_classes = n_classes
+        self.n_channels = n_channels
+
+        self.conv1 = Conv(3, 64)
+
+        self.max_pool1 = nn.MaxPool2d(3, stride=2)
+
+        self.conv2_x = ResidualBlock(64, 64, 256)
+
+        self.conv3_x = ResidualBlock(256, 128, 512)
+
+        self.conv4_x = ResidualBlock(512, 256, 1024)
+
+        self.conv5_x = ResidualBlock(1024, 512, 2048)
+
+        self.deconv_layers = 
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.max_pool1(x)
+        for _ in range(3):
+            x = self.conv2_x(x)
+        for _ in range(4):
+            x = self.conv3_x(x)
+        for _ in range(6):
+            x = self.conv4_x(x)
+        for _ in range(3):
+            x = self.conv5_x(x)
+        x = self.avgPool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        x = nn.Softmax(x)
+        return x
+    
+    def get_deconv_cfg(self, deconv_kernel, index):
+        if deconv_kernel == 4:
+            padding = 1
+            output_padding = 0
+        elif deconv_kernel == 3:
+            padding = 1
+            output_padding = 1
+        elif deconv_kernel == 2:
+            padding = 0
+            output_padding = 0
+        return deconv_kernel, padding, output_padding
+
+    def make_deconv_layer(self, num_layers, filters, kernels ):
+        layers = []
+        for i in range(num_layers):
+            deconv_kerenl, padding, output_padding = self.get_deconv_cfg(kernels, i)
+
+            layers.append(nn.ConvTranspose2d())
+
+
         
 
 
