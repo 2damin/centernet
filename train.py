1ae5a2d1b802d75a0f6e799f96328a54433a259e
dmlee <dmlee@markany.com>
2021-06-15 18:41:54 +0900

    edit train process

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 import argparse
+import torch
+import cv2
+
 from dataloader import dataloader
 from model import model
+from torch.utils.data import DataLoader
 
 def read_data_file(path):
     options = dict()
@@ -15,7 +19,30 @@ def read_data_file(path):
     return options
 
 
-def train(dataset, model):
+def train(trainDataset,
+          validDataset,
+          model,
+          batch_size: int = 1,
+          shuffle: bool = True,
+          num_workers: int = 0):
+
+    train_loader = DataLoader(trainDataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=1)
+
+    device = torch.device("cuda")
+
+    model.to(device=device)
+    model.eval()
+
+    print(len(train_loader))
+
+    for batch in train_loader:
+        x = batch['image']
+        x = x.to(device=device, dtype=torch.float32)
+        pred = model(x)
+        #print(x)
+        #cv2.imwrite("x.jpg",x.numpy())
+        #pred = model()
+    
     return model
 
 if __name__ == "__main__":
@@ -24,18 +51,20 @@ if __name__ == "__main__":
     data_config = read_data_file("./centernet.data")
     print(" read data_config file ")
 
-    dataset = dataloader.YOLODataset(data_config['valid'],
+    trainDataset = dataloader.YOLODataset(data_config['train'],
                                   img_w = 608,
                                   img_h = 608,
                                   use_augmentation = True)
 
+    validDataset = dataloader.YOLODataset(data_config['valid'],
+                                  img_w = 608,
+                                  img_h = 608,
+                                  use_augmentation = False)
+
     print( "load dataset ")
 
-    model = model.ResNet(3, 2)
+    model = model.CenterNet_ResNet(3, 2)
 
-    # for idx in range(len(dataset.imgs_path)):
-    #     dataset.__getitem__(idx)
-
-    train(dataset, model)
+    train(trainDataset, validDataset, model, batch_size = 1, shuffle = True, num_workers = 1)
 
     print("done")
