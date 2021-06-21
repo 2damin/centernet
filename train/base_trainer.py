41b4eef19e8244b55a56c3b8b9ffeba6f05f412e
2damin <leesak2003@naver.com>
2021-06-21 12:13:36 +0000

    add trainer code

---
+++ train/base_trainer.py
@@ -0,0 +1,68 @@
+import time
+import torch
+from utils.utils import AverageMeter
+from progress.bar import Bar
+
+
+class ModelWithLoss(torch.nn.Module):
+    def __init__(self, model, loss):
+        super(ModelWithLoss, self).__init__()
+        self.model = model
+        self.loss = loss
+    
+    def forward(self, batch):
+        outputs = self.model(batch['image'])
+        loss, loss_stats = self.loss(outputs, batch)
+        return outputs[-1], loss, loss_stats
+    
+class BaseTrainer(object):
+    def __init__(self, opt, model, optimizer=None):
+        self.opt = opt
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_stats, self.loss = self._get_losses(opt)
+        self.model_with_loss = ModelWithLoss(model, self.loss)
+
+    def set_device(self, gpus, chunk_sizes, device):
+        self.model_with_loss = self.model_with_loss.to(device)
+
+        for state in self.optimizer.state.values():
+            for k, v in state.items():
+                if isinstance(v, torch.Tensor):
+                    state[k] = v.to(device=device, non_blocking=True)
+
+    def run_epoch(self, phase, epoch, data_loader):
+        model_with_loss = self.model_with_loss
+        if(phase == 'train'):
+            model_with_loss.train()
+        else:
+            model_with_loss.eval()
+            torch.cuda.empty_cache()
+        
+        opt = self.opt
+        results = {}
+        data_time, batch_time = AverageMeter(), AverageMeter()
+        avg_loss_stats = {l: AverageMeter() for l in self.loss_stats}
+        num_iters = len(data_loader) if opt.num_iters < 0 else opt.num_iters
+        bar = Bar('{}/{}'.format(opt.task, opt.exp_id), max=num_iters)
+        end = time.time()
+        
+        for iter_id, batch in enumerate(data_loader):
+            if iter_id >= num_iters:
+                break
+            data_time.update(time.time() - end)
+
+            for k in batch:
+                if k != 'meta':
+                    batch[k] = batch[k].to(device=opt.device, non_blocking=True)
+            output, loss, loss_stats = model_with_loss(batch)
+            loss = loss.mean()
+            if phase == 'train':
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+    def _get_losses(self, opt):
+        raise NotImplementedError
