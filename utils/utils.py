ae83b7e7fa2bc65188229a04df61f0a2afda2a26
2damin <leesak2003@naver.com>
2021-06-29 09:06:12 +0000

    update

---
+++ utils/utils.py
@@ -0,0 +1,22 @@
+import torch
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        if self.count > 0:
+            self.avg = self.sum / self.count
+
+def _sigmoid(x):
+    y = torch.clamp(torch.sigmoid(x), min=1e-4, max=1-1e-4)
+    return y
